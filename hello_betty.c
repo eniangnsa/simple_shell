f5c6079950d4f46549b366532c3f0c4f4ce667e4
eniangnsa <adolph.eniang@gmail.com>
2022-11-16 00:21:14 -0800

    print my parent PID

--- hello_betty.c
+++ hello_betty.c
@@ -9,4 +9,19 @@ int main(void)
 {
 	printf("Hello Betty");
 	return 0;
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PPID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_ppid;
+
+    my_ppid = getppid();
+    printf("%u\n", my_ppid);
+    return (0);
 }	
